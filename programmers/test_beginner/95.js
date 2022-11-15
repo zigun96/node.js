f4e0c3143fcc2a6ea92af5605bd321f661071486
cha_jae_geon <jaegun920@naver.com>
2022-11-15 18:30:14 +0900

    프로그래머스 입문자 문제풀이 commit

--- programmers/test_beginner/95.js
+++ programmers/test_beginner/95.js
@@ -1,21 +1,13 @@
+var answer = [];
+
 function solution(num, total) {
-  var answer = [];
-  if (num % 2 == 1){
-      var odd_center = parseInt(total / num);
-      var ii = odd_center;
-      var rr = odd_center;
-      for (var i = 0; i <= num / 2; i++){
-          answer[i] = ii--;
-      }
-      for (var i = (num / 2) + 1; i < num; i++){
-          answer[i] = ++rr;
-      }
+  var half_num = num / 2;
+  var center = total / num;
+  var init_num = Math.ceil(center - half_num);
+  
+  for (var i = 0; i < num; i++){
+      answer[i] = init_num++;
   }
-  
-//     else if (num % 2 != 1){
-//         var even_center = Math.floor(total / 2);
-//     }
-  
   return answer;
 }
 solution(3,12)
@@ -29,4 +21,3 @@ console.log(answer);
 
 solution(5,5)
 console.log(answer);
-
