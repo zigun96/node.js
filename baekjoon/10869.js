6478b429d1bc11bd5dbb006e185c974192b5c587
cha_jae_geon <jaegun920@naver.com>
2022-11-15 17:09:41 +0900

    백준알고리즘 10869번 commit

--- baekjoon/10869.js
+++ baekjoon/10869.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
-let [a, b] = fs.readFileSync('/index.txt').toString().split(' ');
+// Control + D 로 콘솔입력 끝내기
+let [a, b] = fs.readFileSync('/dev/stdin').toString().split(' ');
 a = parseInt(a);
 b = parseInt(b);
+// A+B 출력
+console.log(a + b);
+// A-B 출력
+console.log(a - b);
+// A*B 출력
+console.log(a * b);
+// A/B 출력
+console.log(parseInt(a / b));
+// A%B(나머지) 출력
+console.log(a % b);
