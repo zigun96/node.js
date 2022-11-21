89742e6436b1f3f617569dad240468f36bba9516
cha_jae_geon <jaegun920@naver.com>
2022-11-21 17:29:16 +0900

    프로그래머스 입문자 문제풀이 commit

---
+++ programmers/test_beginner/24.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    for (let i = 1 ; i < n; i++) {
+        if( 6 * i % n == 0){
+            return i;
+        }
+    }
+    return answer = n;
+}
+
+console.log(solution(6));
+console.log(solution(10));
+console.log(solution(4));
