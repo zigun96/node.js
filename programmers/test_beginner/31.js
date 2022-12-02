6ac6b0c825d83b8daf88ee73ad3d231461fc25f9
cha_jae_geon <jaegun920@naver.com>
2022-12-02 17:22:12 +0900

    프로그래머스 입문자 문제풀이 수정 commit

--- programmers/test_beginner/31.js
+++ programmers/test_beginner/31.js
@@ -1,5 +1,5 @@
 // 문자 반복 출력하기
-function solution(my_string, n) {
+function solution1(my_string, n) {
   var answer = '';
   var arr1 = [...my_string];
   var arr2 = [];    
@@ -11,5 +11,29 @@ function solution(my_string, n) {
   answer = arr2.join("");
   return answer;
 }
+// function solution2(my_string, n) {
+//   var answer = '';
+//   for(let i = 0; i < my_string.length; i++){
+//       // answer += my_string.charAt(i).repeat(n);
+//       let char = my_string[i];            
+//       for(let j = 0; j < n; j++){
+//           answer += char;
+//       }
+//   }
+//   return answer;
+// }
+
+// function solution3(my_string, n) {
+//   return [...my_string].map((char) => char.repeat(n)).join('');
+//   return my_string.split('').map((char) => char.repeat(n)).join('');
+// }
+
+// function solution4(my_string, n) {
+//   let answer = '';
+//   for (let i = 0; i < my_string.length; i++) {
+//     answer += my_string.charAt(i).repeat(n);
+//   }
+//   return answer;
+// }
 
 console.log(solution("hello",3));
