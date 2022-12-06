bab2dbaaa45755596bf7b8af679fb001c9ad858a
cha_jae_geon <jaegun920@naver.com>
2022-12-06 18:30:47 +0900

    프로그래머스 입문자 문제풀이 수정 commit

--- programmers/test_beginner/42.js
+++ programmers/test_beginner/42.js
@@ -1,7 +1,6 @@
 // 문자열 뒤집기
 function solution(my_string) {
-  let arr = [...my_string];
-  return arr.reverse().join("");
+  return [...my_string].reverse().join("");
 }
 
 console.log(solution("jaron"));
