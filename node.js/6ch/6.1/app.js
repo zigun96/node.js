e3c32e9e59675b386eec6a5634628aabc089cf93
cha_jae_geon <jaegun920@naver.com>
2023-01-02 17:19:29 +0900

    chatting 코드 수정

--- node.js/6ch/6.1/app.js
+++ node.js/6ch/6.1/app.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const path =require('path');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
-
-// app.get('/', (req, res) => {
-//   res.sendFile(`${__dirname}/index.html`);
-// });
+app.set('port', process.env.PORT || 8888);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join((__dirname), '/index.html'));
+    res.send('Hello, Express');
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).send(err.message);
-})
-
-app.listen(app.get('port'), () => {
-    let port = app.get('port');
+app.listen(port, () => {
     console.log(`http://localhost:${port} 서버 대기중`);
 });
